40ebc08df841247b782296f45468d21992357b09
Abhishek Sharma <abhishek0058@gmail.com>
2019-12-23 16:55:47 +0530

    created sample file for simple linear regression

--- Part 2 - Regression/Section 4 - Simple Linear Regression/work/simple_regresssion.py
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/work/simple_regresssion.py
@@ -20,3 +20,26 @@ from sklearn.model_selection import train_test_split
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
 
+
+from sklearn.linear_model import LinearRegression
+
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+y_pred_train = regressor.predict(X_train)
+y_pred = regressor.predict(X_test)
+
+
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_train, y_pred_train, color = 'blue')
+plt.title("Salary V Experience (Training Set)")
+plt.xlabel("Years of experince")
+plt.ylabel("Salary")
+plt.show()
+
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_test, y_pred, color = 'blue')
+plt.title("Salary V Experience (Test Set)")
+plt.xlabel("Years of experince")
+plt.ylabel("Salary")
+plt.show()
